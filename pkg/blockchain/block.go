7e87248fd509e51025141bd0731bf2ffa23e1b7c
João Vitor Espig <jotinha1300@gmail.com>
2022-10-25 23:07:18 -0300

    added serialize and deserialize methods

--- pkg/blockchain/block.go
+++ pkg/blockchain/block.go
@@ -1,11 +1,47 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 // Block represents a block in a blockchain
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
 	Nonce    int
+}
+
+// SerializedBlock represents a block that has passes through the process
+// of serializing
+type SerializedBlock []byte
+
+// Serialize returns a block struct as a SerializedBlock
+func (b *Block) Serialize() SerializedBlock {
+	var result bytes.Buffer
+
+	encoder := gob.NewEncoder(&result)
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// Deserialize transforms a SerializedBlock into a Block
+func (data SerializedBlock) Deserialize() *Block {
+	block := &Block{}
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return block
 }
 
 // NewBlock creates a new block struct
