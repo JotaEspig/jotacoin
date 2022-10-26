51332ce161a00c2b19361c8e1243dd9fc8759d99
João Vitor Espig <jotinha1300@gmail.com>
2022-10-26 15:44:50 -0300

    badger logger disabled

--- pkg/blockchain/db_connection.go
+++ pkg/blockchain/db_connection.go
@@ -4,6 +4,7 @@ import "github.com/dgraph-io/badger"
 
 func connectDB() *badger.DB {
 	opts := badger.DefaultOptions(dbPath)
+	opts.Logger = nil
 	db, err := badger.Open(opts)
 	if err != nil {
 		panic(err)
