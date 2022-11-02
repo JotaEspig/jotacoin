a41812f11324f20b59b3f492e2a6937d16cf5188
João Vitor Espig <jotinha1300@gmail.com>
2022-11-02 11:55:38 -0300

    added address method

--- pkg/wallet/wallet.go
+++ pkg/wallet/wallet.go
@@ -6,6 +6,7 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 
+	"github.com/mr-tron/base58/base58"
 	"golang.org/x/crypto/ripemd160"
 )
 
@@ -14,12 +15,28 @@ const (
 	version        = byte(0x00)
 )
 
-// Wallet stores the Private key and the public key
+// Wallet stores the Private key and the public key. More info at:
+// https://blocktrade.com/wallet-addresses-public-and-private-keys-explained/
 type Wallet struct {
 	PrivateKey *ecdsa.PrivateKey
 	PublicKey  []byte
 }
 
+// Address gets the address of the wallet
+func (w *Wallet) Address() ([]byte, error) {
+	pubHash, err := PublicKeyHash(w.PublicKey)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	versionedHash := append([]byte{version}, pubHash...)
+	checksumVal := checksum(versionedHash)
+
+	fullHash := append(versionedHash, checksumVal...)
+	return []byte(base58.Encode(fullHash)), nil
+}
+
+// NewWallet creates a new wallet with random keys
 func NewWallet() (*Wallet, error) {
 	private, pub, err := NewKeyPair()
 	if err != nil {
@@ -29,6 +46,7 @@ func NewWallet() (*Wallet, error) {
 	return &Wallet{private, pub}, nil
 }
 
+// NewKeyPair generates the private and the public key randomly
 func NewKeyPair() (*ecdsa.PrivateKey, []byte, error) {
 	curve := elliptic.P256()
 	private, err := ecdsa.GenerateKey(curve, rand.Reader)
@@ -40,6 +58,7 @@ func NewKeyPair() (*ecdsa.PrivateKey, []byte, error) {
 	return private, pub, err
 }
 
+//PublicKeyHash generates the hash of the public key
 func PublicKeyHash(pubKey []byte) ([]byte, error) {
 	pubHash := sha256.Sum256(pubKey)
 
@@ -53,7 +72,7 @@ func PublicKeyHash(pubKey []byte) ([]byte, error) {
 	return publicRipMD, nil
 }
 
-func Checksum(payload []byte) []byte {
+func checksum(payload []byte) []byte {
 	firstHash := sha256.Sum256(payload)
 	secondHash := sha256.Sum256(firstHash[:])
 
