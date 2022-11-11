23735b36e4c9c60d81b592d496ad3a7a5920d4c5
João Vitor Espig <jotinha1300@gmail.com>
2022-11-11 15:21:07 -0300

    added test for transaction. Not passing yet

---
+++ tests/transaction_test.go
@@ -0,0 +1,21 @@
+package tests
+
+import (
+	"jotacoin/pkg/blockchain"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewTransaction(t *testing.T) {
+	chain, err := blockchain.ContinueBlockChain()
+	if err != nil {
+		panic(err)
+	}
+	defer chain.DB.Close()
+	tx, err := blockchain.NewTransaction(address1, address2, 1, chain)
+	assert.Equal(t, nil, err)
+
+	isValid := tx.Verify()
+	assert.Equal(t, true, isValid)
+}
