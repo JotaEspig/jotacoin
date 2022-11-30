53bc446eb7caad85d264f798a67da934b7c1a368
João Vitor Espig <90321586+JotaEspig@users.noreply.github.com>
2022-11-30 21:37:14 +0000

    added new cli commands and improved print command

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,11 +1,19 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"jotacoin/pkg/blockchain"
 	"jotacoin/pkg/wallet"
 	"os"
+	"strconv"
 )
+
+func handleError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 // CommandLine is the struct that is responsable for running the commands
 type CommandLine struct {
@@ -14,31 +22,23 @@ type CommandLine struct {
 func (cli *CommandLine) newWallet() {
 	ws, err := wallet.LoadFile()
 	if err != nil {
-		panic(err)
+		ws = wallet.Wallets{}
 	}
 	w, err := wallet.NewWallet()
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 	address, err := w.Address()
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 
 	ws[address] = w
 	err = ws.SaveFile()
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 
 	fmt.Printf("Added Wallet!\nAddress: %s\n", address)
 }
 
 func (cli *CommandLine) showWallets() {
 	ws, err := wallet.LoadFile()
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 	for _, w := range ws {
 		address, err := w.Address()
 		if err != nil {
@@ -48,36 +48,74 @@ func (cli *CommandLine) showWallets() {
 	}
 }
 
+func (cli *CommandLine) getBalance(address string) {
+	chain, err := blockchain.ContinueBlockchain()
+	handleError(err)
+	ws, err := wallet.LoadFile()
+	handleError(err)
+	w := ws.GetWallet(address)
+	if w == nil {
+		panic(errors.New("wallet does not exists"))
+	}
+	pubHash, err := wallet.PublicKeyHash(w.PublicKey)
+	handleError(err)
+	balance := chain.GetBalance(pubHash)
+	fmt.Printf("Balance: %d\n", balance)
+}
+
+func (cli *CommandLine) newTransaction(from, to string, amount int) {
+	chain, err := blockchain.ContinueBlockchain()
+	handleError(err)
+
+	tx, err := blockchain.NewTransaction(from, to, amount, chain)
+	handleError(err)
+	err = chain.AddBlock([]*blockchain.Transaction{tx})
+	handleError(err)
+
+	fmt.Printf("Transaction done!\nTx Hash: %x\nInputs: %v\nOutputs: %v\n\n",
+		tx.HashID, tx.Inputs, tx.Outputs)
+}
+
 func (cli *CommandLine) newBlockchain(address string) {
 	_, err := blockchain.NewBlockchain(address)
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 
 	fmt.Println("New BlockChain created")
 }
 
 func (cli *CommandLine) printAll() {
-	var block *blockchain.Block
-	var err error
-
 	chain, err := blockchain.ContinueBlockchain()
-	if err != nil {
-		panic(err)
-	}
+	handleError(err)
 
+	// Go through all the blocks created
 	iter := chain.Iterator()
 	for {
-		block, err = iter.Next()
+		block, err := iter.Next()
 		if err != nil {
 			break
 		}
 
-		pow := blockchain.NewProof(block)
-		isValid := pow.IsValid()
+		fmt.Printf("Block hash: %x\n\n", block.Hash)
+		for _, tx := range block.Transactions {
+			fmt.Printf("Transaction Hash: %x\n\n", tx.HashID)
 
-		fmt.Println()
-		fmt.Println(*block, isValid)
+			fmt.Println("INPUTS:")
+			for _, in := range tx.Inputs {
+				fmt.Printf("PrevTxHash: %x\nOutIdx: %d\nSig: %x\n",
+					in.PrevTxHash, in.OutIdx, in.Signature)
+			}
+
+			fmt.Println("\nOUTPUTS:")
+			for _, out := range tx.Outputs {
+				fmt.Printf("Amount: %d\nPubKey: %x\n", out.Value, out.PubKeyHash)
+			}
+
+			pow := blockchain.NewProof(block)
+			isValid := pow.IsValid()
+
+			fmt.Printf("is valid?: %t", isValid)
+			fmt.Printf("\n==================\n\n")
+		}
 	}
 }
 
@@ -88,9 +126,19 @@ func (cli *CommandLine) Run() {
 		cli.newWallet()
 	case "showwallets":
 		cli.showWallets()
+	case "getbalance":
+		cli.getBalance(os.Args[2])
+	case "newtransaction":
+		amount, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			panic(err)
+		}
+		cli.newTransaction(os.Args[2], os.Args[3], amount)
 	case "newblockchain":
 		cli.newBlockchain(os.Args[2])
 	case "print":
 		cli.printAll()
+	default:
+		fmt.Println("Command not found")
 	}
 }
